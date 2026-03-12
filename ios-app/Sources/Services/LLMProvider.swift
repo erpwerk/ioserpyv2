57e5ba600b586c4ead0927ed87911017352ffd03
erpwerk <erpwerk09@gmail.com>
2026-03-12 11:27:51 +0100

    Delete ios-app/Sources directory

--- ios-app/Sources/Services/LLMProvider.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-protocol LLMProvider {
-    func generateStream(prompt: String, model: String, apiKey: String, onMessage: @escaping (String) -> Void, onComplete: @escaping () -> Void)
-}
